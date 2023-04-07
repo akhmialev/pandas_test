c24be26eb83307818db4f594b2e42e6c8d858b6f
artem <akhmialev@gmail.com>
2023-04-07 11:10:08 +0300

    added new config data

--- telegram_bot/practic/work_bot/test_data.py
+++ telegram_bot/practic/work_bot/test_data.py
@@ -1,26 +1,205 @@
-import datetime
-
 trainers = [
     {
-        'name': 'Андрей',
-        'lname': 'Андреевич',
-        'time': [],
-        'week_day': ['02.04', '05.04', '24.04', '20.04'],
-        'clients': [
-            {
-                'telegram_id': "",
-                'first_name': '',
-                'username': '',
-                'record_date': '',
-                'record_time': '',
-                'check_click': "2023-03-31",
+        "name": "Андрей",
+        "last_name": "Андреевич",
+        "id_telegram": "",
+        "records": [{
+            "id_user": "",
+            "date": "",
+            "time": "",
+            "status": ""
+        }],
+        "working_schedule": {
+            "weekend_days": [],
+            "work_days": [
+                {
+                    "date": "",
+                    "time": ""
+                }
+            ]
+        },
+        "rating": {
+            "id_user": "",
+            "rating": ""
+        },
+        "grade": {
+            "title": "",
+            "description": "",
+            "date": "",
+        },
+        "photo_url": ""
+    },
+   {
+        "name": "Иван",
+        "last_name": "Иванович",
+        "id_telegram": "",
+        "records": [{
+            "id_user": "",
+            "date": "",
+            "time": "",
+            "status": ""
+        }],
+        "working_schedule": {
+            "weekend_days": [],
+            "work_days": [
+                {
+                    "date": "06.04",
+                    "time": "10-15"
+                },
+                {
+                    "date": "07.04",
+                    "time": "10-15"
+                },
+                {
+                    "date": "08.04",
+                    "time": "10-15"
+                },
+                {
+                    "date": "09.04",
+                    "time": "11-15"
+                },
+                {
+                    "date": "10.04",
+                    "time": "12-15"
+                },
+                {
+                    "date": "11.04",
+                    "time": "13-15"
+                },
+                {
+                    "date": "12.04",
+                    "time": "5-15"
+                },
+                {
+                    "date": "13.04",
+                    "time": "6-15"
+                },
+                {
+                    "date": "14.04",
+                    "time": "7-15"
+                },
+                {
+                    "date": "15.04",
+                    "time": "10-15"
+                },
+                {
+                    "date": "16.04",
+                    "time": "9-15"
+                },
+                {
+                    "date": "17.04",
+                    "time": "10-14"
+                },
+                {
+                    "date": "18.04",
+                    "time": "10-16"
+                },
+                {
+                    "date": "19.04",
+                    "time": "10-17"
+                },
+                {
+                    "date": "20.04",
+                    "time": "10-18"
+                },
+                {
+                    "date": "21.04",
+                    "time": "10-19"
+                },
+                {
+                    "date": "22.04",
+                    "time": "7-20"
+                },
+                {
+                    "date": "23.04",
+                    "time": "10-15"
+                },
+                {
+                    "date": "24.04",
+                    "time": "8-21"
+                },
+                {
+                    "date": "25.04",
+                    "time": "9-22"
+                },
+                {
+                    "date": "26.04",
+                    "time": "10-23"
+                },
+                {
+                    "date": "28.04",
+                    "time": "10-15"
+                },
+                {
+                    "date": "29.04",
+                    "time": "10-15"
+                },
+                {
+                    "date": "30.04",
+                    "time": "10-15"
+                },
+                {
+                    "date": "01.05",
+                    "time": "10-15"
+                },
+                {
+                    "date": "02.05",
+                    "time": "10-15"
+                },
+                {
+                    "date": "03.05",
+                    "time": "10-15"
+                },
+                {
+                    "date": "04.05",
+                    "time": "10-15"
+                },
+                {
+                    "date": "05.05",
+                    "time": "10-15"
+                },
+            ]
+        },
+        "rating": {
+            "id_user": "",
+            "rating": ""
+        },
+        "grade": {
+            "title": "",
+            "description": "",
+            "date": "",
+        },
+        "photo_url": ""
+    }
+]
 
+users = [
+    {
+        "username": "",
+        "first_name": "",
+        "id_telegram": "",
+        "trainers": [{
+            "id_trainers": "",
+            "status": "",
+        }],
+        "gym": [{
+            "id_gym": "",
+            "status_gym": "",
+        }]
+    }
+]
+
+gyms = [
+    {
+        "title": '',
+        'descriptions': '',
+        'city': '',
+        'address': '',
+        'trainers': [
+            {
+                'id': '',
+                'status': ''
             }
         ]
     }
 ]
-
-a = {
-    510329688 : '2023-03-31',
-
-}
